c09dbad3db09b7830bb41290db429e19f4f5a072
Dan Motzenbecker <dan@oxism.com>
2015-08-30 00:27:51 -0400

    update clojure dependencies to latest

--- resources/project.clj
+++ resources/project.clj
@@ -4,10 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "0.0-3308"]
-                 [org.omcljs/om "0.8.8"]
+                 [org.clojure/clojurescript "1.7.122"]
+                 [org.omcljs/om "0.9.0"]
                  [org.omcljs/ambly "0.6.0"]]
-:plugins [[lein-cljsbuild "1.0.6"]]
+:plugins [[lein-cljsbuild "1.1.0"]]
 :cljsbuild {:builds {:dev {:source-paths ["src"]
                            :compiler {:output-to "target/out/main.js"
                                       :output-dir "target/out"
