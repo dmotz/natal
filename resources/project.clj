d446960264a60e825070594fa8ec94ff0c9cb8ca
Dan Motzenbecker <dan@oxism.com>
2015-11-15 23:56:16 -0500

    use natal-shell

--- resources/project.clj
+++ resources/project.clj
@@ -6,9 +6,10 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
                  [$REACT_INTERFACE$]
-                 [org.omcljs/ambly "0.6.0"]]
-:plugins [[lein-cljsbuild "1.1.0"]]
-:cljsbuild {:builds {:dev {:source-paths ["src"]
-                           :compiler {:output-to "target/out/main.js"
-                                      :output-dir "target/out"
-                                      :optimizations :none}}}})
+                 [org.omcljs/ambly "0.6.0"]
+                 [natal-shell "0.1.0"]]
+ :plugins [[lein-cljsbuild "1.1.0"]]
+ :cljsbuild {:builds {:dev {:source-paths ["src"]
+                            :compiler {:output-to "target/out/main.js"
+                                       :output-dir "target/out"
+                                       :optimizations :none}}}})
