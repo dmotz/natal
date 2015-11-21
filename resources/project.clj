6267875babbcb27a3d10e6791174a4d1e0356432
Dan Motzenbecker <dan@oxism.com>
2015-11-21 16:23:55 -0500

    updated clj deps

--- resources/project.clj
+++ resources/project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.145"]
+                 [org.clojure/clojurescript "1.7.170"]
                  [$REACT_INTERFACE$]
                  [org.omcljs/ambly "0.6.0"]
                  [natal-shell "0.1.0"]]
- :plugins [[lein-cljsbuild "1.1.0"]]
+ :plugins [[lein-cljsbuild "1.1.1"]]
  :cljsbuild {:builds {:dev {:source-paths ["src"]
                             :compiler {:output-to "target/out/main.js"
                                        :output-dir "target/out"
