2e7ebb43e2da1e6c4e60c39db8b80f6cb5263766
Dan Motzenbecker <dan@oxism.com>
2015-11-23 23:41:45 -0500

    added try catch to om template with error feedback

--- resources/om.cljs
+++ resources/om.cljs
@@ -11,18 +11,32 @@
   (reify
     om/IRender
     (render [this]
-      (view {:style {:flexDirection "column" :margin 40 :alignItems "center"}}
-        (text
-          {:style {:fontSize 50 :fontWeight "100" :marginBottom 20 :textAlign "center"}}
-          (:text data))
+      (try
+        (view {:style {:flexDirection "column" :margin 40 :alignItems "center"}}
+          (text
+            {:style {:fontSize 50 :fontWeight "100" :marginBottom 20 :textAlign "center"}}
+            (:text data))
 
-        (image {:source {:uri "https://raw.githubusercontent.com/cljsinfo/logo.cljs/master/cljs.png"}
-                :style {:width 80 :height 80 :marginBottom 30}})
+          (image {:source {:uri "https://raw.githubusercontent.com/cljsinfo/logo.cljs/master/cljs.png"}
+                  :style {:width 80 :height 80 :marginBottom 30}})
 
-        (touchable-highlight
-          {:style {:backgroundColor "#999" :padding 10 :borderRadius 5}
-           :onPress #(alert "HELLO!")}
-          (text {:style {:color "white" :textAlign "center" :fontWeight "bold"}} "press me"))))))
+          (touchable-highlight
+            {:style {:backgroundColor "#999" :padding 10 :borderRadius 5}
+             :onPress #(alert "HELLO!")}
+            (text {:style {:color "white" :textAlign "center" :fontWeight "bold"}} "press me")))
+
+        (catch :default e
+          (view {:style {:backgroundColor "#cc0814" :flex 1 :padding 20 :paddingTop 40}}
+            (text
+              {:style {:fontWeight "normal" :color "white" :fontSize 24 :marginBottom 10}}
+              "ERROR")
+            (text
+              {:style {:color "white" :fontFamily "Menlo-Regular" :fontSize 16 :lineHeight 24}}
+              (.-message e))
+            (text
+              {:style {:color "white" :marginTop 20 :fontSize 16 :fontWeight "bold"}}
+              "Check REPL log for details.")))))))
+
 
 (om/root widget app-state {:target 1})
 
