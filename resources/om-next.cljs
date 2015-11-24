1607f651ac076502a83847aa10510d7e1c354b1b
Dan Motzenbecker <dan@oxism.com>
2015-11-23 23:48:18 -0500

    added try catch to om-next template with error feedback

--- resources/om-next.cljs
+++ resources/om-next.cljs
@@ -13,21 +13,34 @@
          '[:app/msg])
   Object
   (render [this]
-    (let [{:keys [app/msg]} (om/props this)]
-      (view {:style {:flexDirection "column" :margin 40 :alignItems "center"}}
-        (text
-          {:style {:fontSize 50 :fontWeight "100" :marginBottom 20 :textAlign "center"}}
-          msg)
+    (try
+      (let [{:keys [app/msg]} (om/props this)]
+        (view {:style {:flexDirection "column" :margin 40 :alignItems "center"}}
+          (text
+            {:style {:fontSize 50 :fontWeight "100" :marginBottom 20 :textAlign "center"}}
+            msg)
 
-        (image {:source {:uri "https://raw.githubusercontent.com/cljsinfo/logo.cljs/master/cljs.png"}
-                :style {:width 80 :height 80 :marginBottom 30}})
+          (image {:source {:uri "https://raw.githubusercontent.com/cljsinfo/logo.cljs/master/cljs.png"}
+                  :style {:width 80 :height 80 :marginBottom 30}})
 
-        (touchable-highlight
-          {:style {:backgroundColor "#999" :padding 10 :borderRadius 5}
-           :onPress #(alert "HELLO!")}
-          (text {:style {:color "white" :textAlign "center" :fontWeight "bold"}} "press me"))))))
+          (touchable-highlight
+            {:style {:backgroundColor "#999" :padding 10 :borderRadius 5}
+             :onPress #(alert "HELLO!")}
+            (text {:style {:color "white" :textAlign "center" :fontWeight "bold"}} "press me"))))
 
-;; om.next parser
+      (catch :default e
+        (view {:style {:backgroundColor "#cc0814" :flex 1 :padding 20 :paddingTop 40}}
+          (text
+            {:style {:fontWeight "normal" :color "white" :fontSize 24 :marginBottom 10}}
+            "ERROR")
+          (text
+            {:style {:color "white" :fontFamily "Menlo-Regular" :fontSize 16 :lineHeight 24}}
+            (.-message e))
+          (text
+            {:style {:color "white" :marginTop 20 :fontSize 16 :fontWeight "bold"}}
+            "Check REPL log for details."))))))
+
+
 (defmulti read om/dispatch)
 (defmethod read :default
   [{:keys [state]} k _]
