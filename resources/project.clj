1136600a100842d82275f559deaf7eff7a1a53cb
Dan Motzenbecker <dan@oxism.com>
2015-12-04 00:39:20 -0500

    update natal-shell version

--- resources/project.clj
+++ resources/project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/clojurescript "1.7.170"]
                  [$REACT_INTERFACE$]
                  [org.omcljs/ambly "0.6.0"]
-                 [natal-shell "0.1.0"]]
+                 [natal-shell "0.1.2"]]
  :plugins [[lein-cljsbuild "1.1.1"]]
  :cljsbuild {:builds {:dev {:source-paths ["src"]
                             :compiler {:output-to "target/out/main.js"
